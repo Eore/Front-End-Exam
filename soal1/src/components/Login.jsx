7736da652570c97b65bf7fec2bdb8678c98c6d9c
eore <eore26@gmail.com>
2018-03-07 14:26:05 +0700

    update

--- soal1/src/components/Login.jsx
+++ soal1/src/components/Login.jsx
@@ -6,10 +6,11 @@ export default class Login extends React.Component {
         <div className="render form-group">
             <label htmlFor="login">Nama</label>
             <input type="text" ref="login" id="login" className="form-control"/>
-            <Link to='/welcome' onClick={() => {
-                this.props.changeActive(1);
-                this.props.changeName(this.refs.login.value);
-            }}  
+            <Link to='/welcome' 
+                onClick={() => {
+                    this.props.changeActive(1);
+                    this.props.changeName(this.refs.login.value);
+                }}  
             className="btn btn-primary">Submit</Link>
         </div>
 }
